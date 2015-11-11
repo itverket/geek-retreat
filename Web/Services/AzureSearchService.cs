b93f30fbddacb0de7904e3d0d063bf6822bedbc0
Kato Stølen <kato.stoelen@gmail.com>
2015-11-11 20:25:09 +0100

    Added back-end of Web

---
+++ Web/Services/AzureSearchService.cs
@@ -0,0 +1,41 @@
+﻿using Entities.Twitter.SearchIndex;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Framework.Configuration;
+using System.Collections.Generic;
+
+namespace Web.Services
+{
+    public class AzureSearchService
+    {
+        private SearchIndexClient _indexClient;
+
+        public AzureSearchService(IConfigurationRoot configurationRoot)
+        {
+            var searchServiceName = configurationRoot["AppSettings:SearchServiceName"];
+            var searchApiKey = configurationRoot["AppSettings:SearchServiceApiKey"];
+            var tweetIndexName = configurationRoot["AppSettings:TweetIndexName"];
+
+            var serviceClient = new SearchServiceClient(
+                searchServiceName, new SearchCredentials(searchApiKey));
+
+            _indexClient = serviceClient.Indexes.GetClient(tweetIndexName);
+        }
+
+        public virtual DocumentSearchResponse<FlattendTweet> Search(
+            string query, string username = null)
+        {
+            var result = _indexClient.Documents.Search<FlattendTweet>(
+                query,
+                new SearchParameters
+                {
+                    OrderBy = new List<string> { "CreatedAt" },
+                    Filter = string.IsNullOrEmpty(username)
+                        ? null
+                        : $"Username eq '{username}'"
+                });
+
+            return result;
+        }
+    }
+}
---
+++ Web/ViewModels/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.ViewModels
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
